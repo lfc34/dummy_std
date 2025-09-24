e208418e587ee8dcb45cd29ba3366dceee36309c
lfc34 <ricardomilos300@yahoo.com>
2025-09-24 21:17:14 +0300

    Implemented exponential function

--- include/dm_math.h
+++ include/dm_math.h
@@ -6,6 +6,13 @@
 #define EULER 2.71828
 #define EPSILON 1e-9
 
+// IEEE 754 standard
+#define NAN 0x7FF8000000000000ULL
+#define INF 0x7FF000000000000ULL
+#define NEG_INF 0xFFF0000000000000ULL
+
+double factorial(int x);
+
 // create a quiet nan
 double dm_gen_quiet_nan();
 
@@ -14,6 +21,12 @@ double dm_gen_inf(int sign);
 
 // return absolute value |x|
 double dm_fabs(double x);
+
+// returns non zero value if x is negative
+int dm_signbit(double x);
+
+// return exponential value of x
+double dm_exp(double x);
 
 // return natural log of x
 double dm_log(double x);
--- src/dm_math.c
+++ src/dm_math.c
@@ -1,9 +1,39 @@
 #include "dm_math.h"
 #include "dm_string.h"
+
 // TODO: add checks for nan, -inf and +inf in each func
 
+// pow used ONLY for positive natural numbers
+static double natural_pow(double base, int exp) {
+    if (exp == 0) {
+        return 1;
+    }
+    if (base == 0 || (base > 0 && base < EPSILON)) {
+        return 0;
+    }
+    double res = 1;
+    for (int i = 0; i < exp; i++) {
+        res *= base;
+    }
+    return res;
+}
+
+double factorial(int x) {
+    if (x == 0 || x == 1 || (x > 0 && x < EPSILON)) {
+        return 1;
+    }
+    if (x < 0) {
+        return dm_gen_quiet_nan();
+    }
+    double res = 1.0f;
+    for (int i = 1; i <= x; i++) {
+        res *= i;
+    }
+    return res;
+}
+
 double dm_gen_quiet_nan() {
-    long long unsigned bits = 0x7ff8000000000000ULL;
+    long long unsigned bits = NAN;
     double nan_val;
     dm_memcpy(&nan_val, &bits, sizeof(nan_val));
     return nan_val;
@@ -14,10 +44,10 @@ double dm_gen_inf(int sign) {
     double inf;
     if (sign == 0) {
         // positive inf
-        bits = 0x7FF0000000000000ULL;
+        bits = INF;
     } else {
         // negative inf
-        bits = 0xFFF0000000000000ULL;
+        bits = NEG_INF;
     }
     dm_memcpy(&inf, &bits, sizeof(inf));
     return inf;
@@ -25,6 +55,7 @@ double dm_gen_inf(int sign) {
 
 double dm_fabs(double x) {
     // naive - better?
+    // not true for -0.0 !!! so bit manipulation is better
     // if (x < 0) return -x;
     // return x;
 
@@ -36,6 +67,16 @@ double dm_fabs(double x) {
     return x;
 }
 
+double dm_exp(double x) {
+    // Taylor series with 20 terms (closest to original exp())
+    double res = 1.0;
+    for (int i = 1; i <= 20; i++) {
+        res += (natural_pow(x, i) / (factorial(i)));
+    }
+    return res;
+}
+
+// blank until I implement other basic functions
 double dm_log(double x) {
     if (x < 0) {
         return dm_gen_quiet_nan();
