f3dbf910f5b71980d92fa75bf5b9caab25f0b4a5
lfc34 <ricardomilos300@yahoo.com>
2025-10-04 19:32:16 +0300

    added header with typedef for convenience

---
+++ include/dm_64types.h
@@ -0,0 +1,14 @@
+// short names for types on 64 bit machine
+
+typedef char byte;
+typedef unsigned char u_byte;
+typedef short i16;
+typedef unsigned short u16;
+typedef int i32;
+typedef unsigned int u32;
+typedef long long i64;
+typedef unsigned long long u64;
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+
